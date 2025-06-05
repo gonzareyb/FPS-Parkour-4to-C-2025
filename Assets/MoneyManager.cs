8aadcab5e1d52371b2b5c898c0220afb02888779
gonzareyb <br0004@est.ort.edu.ar>
2025-06-05 12:10:20 -0300

    Clase 5/6

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+
+    public float money;
+    public UIMANAGER uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+
+    
+    public void UpdateMoneyTxt(float moneyToAdd)
+    {
+        if (money + moneyToAdd < 0)
+        {
+            Debug.Log("No Te Alcanza");
+            return;
+        }
+        money += moneyToAdd;
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+
+}
